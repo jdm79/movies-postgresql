fa186e810fcee608821f11bea638feed4f121047
jdm79 <james.malvern@gmail.com>
2022-06-20 20:38:10 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,97 @@
+import datetime
+import database
+
+# from database import add_movie, get_movies, watch_movie, get_watched_movies, create_tables
+
+menu = """Please select one of the following options:
+1) Add new movie
+2) View upcoming movies
+3) View all movies
+4) Watch a movie
+5) View watched movies
+6) Add new user
+7) Search for a movie
+8) Exit
+
+Your selection: """
+welcome = "Welcome to the watchlist app!"
+
+print(welcome)
+database.create_tables()
+
+
+def prompt_add_movie():
+    title = input("Movie title: ")
+    release_date = input("Release date (dd-mm-YYYY): ")
+    # parse the string into a datetime object
+    parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
+    timestamp = parsed_date.timestamp()
+
+    database.add_movie(title, timestamp)
+
+
+def print_movie_list(heading, movies):
+    print(f"-- {heading} --")
+    for _id, title, release_date in movies:
+        movie_date = datetime.datetime.fromtimestamp(release_date)
+        human_date = movie_date.strftime("%b %d %Y")
+        print(f"{_id}: {title} (released on {human_date})")
+    print("----- \n")
+
+
+def prompt_watch_movie():
+    username = input("Username: ")
+    movie_id = input("Enter Movie ID: ")
+    database.watch_movie(username, movie_id)
+
+
+def prompt_add_user():
+    username = input("Add username: ")
+    database.add_user(username)
+
+
+def prompt_show_watched_movies():
+    username = input("Username: ")
+    movies = database.get_watched_movies(username)
+    if movies:
+        print_movie_list(f"{username}'s watched movies", movies)
+    else:
+        print(f"{username} has not viewed any movies yet #sad.")
+
+
+def prompt_search_movies():
+    search_term = input("Enter search term: ")
+    movies = database.search_movies(search_term)
+    if movies:
+        print_movie_list("Movies found", movies)
+    else:
+        print("No movies found for that search term.")
+
+
+# using walrus operator below to refactor code
+while (user_input := input(menu)) != "8":
+    if user_input == "1":
+        prompt_add_movie()
+
+    elif user_input == "2":
+        movies = database.get_movies(True)
+        print_movie_list("Upcoming", movies)
+
+    elif user_input == "3":
+        movies = database.get_movies()
+        print_movie_list("All", movies)
+
+    elif user_input == "4":
+        prompt_watch_movie()
+
+    elif user_input == "5":
+        prompt_show_watched_movies()
+
+    elif user_input == "6":
+        prompt_add_user()
+
+    elif user_input == "7":
+        prompt_search_movies()
+
+    else:
+        print("Invalid option - please try again")
